ffbda934f0be334f11c427bdc50dfb9d72985e63
nyergler <nyergler@committer.creativecommons.org>
2007-10-19 22:03:12 +0000

    Substitute the running total on make.

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ up_to_date:
 	svn up > /dev/null
 
 template.en_US.js: template.html gen_template_js.py
-	python gen_template_js.py template.html
+	python2.4 gen_template_js.py template.html
 
 clean:
 	rm -f $(shell ls -1 template.*js*)
--- gen_template_js.py
+++ gen_template_js.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2.4
 # This takes template.html and makes a document.write() for it.
 # Later, it could take template.html and make DOM objects instead.
 
@@ -8,7 +8,23 @@ import json
 import os
 
 import sys
+import urllib2
+import locale
 
+def load_total():
+	"""Load the total from the site, return it as a formatted string."""
+
+       	total = urllib2.urlopen(
+            'http://creativecommons.org/includes/total.txt').read().strip()
+        
+        return locale.format('$ %d', locale.atoi(total), True)
+
+def subst_total(in_string):
+	"""Replace occurences of {{total}} in in_string with the current
+	donation total."""
+
+	return in_string.replace("{{total}}", load_total())
+	
 def jsify(in_string):
 	lines = in_string.split('\n')
 	if lines[0].startswith('<?xml version='):
@@ -22,7 +38,7 @@ def main():
 	# Javascript-ify every item passed in
 	for filename in sys.argv[1:]:
 	    file('%s.js' % filename, 'w').write(
-	    		 jsify(file(filename, 'r').read())
+	    		 jsify(subst_total(file(filename, 'r').read()))
 			 )
 
 if __name__ == '__main__':
